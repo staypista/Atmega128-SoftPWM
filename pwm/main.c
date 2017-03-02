ce6883c19936bc4e1f05b831aa5c5b21f0807be5
Nagy Zsolt <stay.pista@gmail.com>
2017-03-02 08:26:05 +0100

    File are readable

--- pwm/main.c
+++ pwm/main.c
@@ -9,23 +9,26 @@
 #include <avr/interrupt.h>
 void init();
 uint8_t i=0;
+
 ISR(TIMER0_COMP_vect){
 	PORTD&=~(1<<7);
 }
+
 ISR(TIMER0_OVF_vect){
-	PORTD|=(1<<7);
-	
+	PORTD|=(1<<7);	
 }
+
 int main(void){
     init();
     for(;;){
 	}
 }
+
 void init(){
 	DDRC|=(1<<PD7);
 	DDRE|=(1<<PE3)|(1<<PE2);
 	DDRD|=(1<<7);
-	TCCR0|=1;
+	TCCR0|=(1<<CS00);
 	TIMSK|=(1<<OCIE0)|(1<<TOIE0);
 	OCR0=250;
 	sei();
